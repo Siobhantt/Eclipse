4dc01d109e9729838f49b83eccc1aab451ce241f
lcardozo <lcardozo@106-02.info2>
2022-09-21 13:18:59 +0200

    Primer Commit

---
+++ CualculoEdadUsuario/src/calculoedadusuario/CalculoEdadUsuario.java
@@ -0,0 +1,18 @@
+package calculoedadusuario;
+
+import java.util.Scanner;
+
+public class CalculoEdadUsuario {
+
+	public static void main(String[] args) {
+		int añoActual;
+		int añoNac;
+		Scanner CalculoEdadUsuario = new Scanner(System.in);
+		
+				
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
