e479941ecc03ee719047eed11974b196296baa7e
lcardozo <lcardozo@106-02.info2>
2022-09-22 14:24:01 +0200

    Edad

--- CualculoEdadUsuario/src/calculoedadusuario/CalculoEdadUsuario.java
+++ CualculoEdadUsuario/src/calculoedadusuario/CalculoEdadUsuario.java
@@ -5,9 +5,22 @@ import java.util.Scanner;
 public class CalculoEdadUsuario {
 
 	public static void main(String[] args) {
-		int añoActual;
-		int añoNac;
-		Scanner CalculoEdadUsuario = new Scanner(System.in);
+		
+		Scanner calculoEdadUsuario = new Scanner(System.in); //el scanner es este, entonces cada vez que quiera scannear algo tengo que colocar el calculoEdadUsuario
+		//meter el scanner en la variable para que lo lea
+		System.out.println("En que año estas?: ");
+		int añoActual = calculoEdadUsuario.nextInt();
+		//Esto es la variable que debe leer un numero (por eso el "calculoedadusuario.nextint"
+		System.out.println("En que año naciste?: ");
+		int añoNac = calculoEdadUsuario.nextInt();
+		int edadPersona =  añoActual - añoNac;
+		System.out.println("Tu edad es: " + edadPersona );
+		
+		calculoEdadUsuario.close();
+		}
+		
+		
+		
 		
 				
 		
@@ -15,4 +28,3 @@ public class CalculoEdadUsuario {
 
 	}
 
-}
