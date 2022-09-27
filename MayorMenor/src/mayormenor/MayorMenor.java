7c08d45a68e25490993ceac868493067f2b4d28b
lcardozo <lcardozo@106-02.info2>
2022-09-27 12:45:12 +0200

    :((

---
+++ MayorMenor/src/mayormenor/MayorMenor.java
@@ -0,0 +1,37 @@
+package mayormenor;
+
+import java.util.Scanner;
+
+public class MayorMenor {
+
+	public static void main(String[] args) {
+		//Realizar una aplicación que solicite al usuario su edad y le indique si es mayor de edad 
+		//(mediante un literal booleano: true o false).
+		 
+		//Zona de Variables
+		int edad;
+		final int MAYORIA_EDAD;
+		int menoriaEdad;
+		
+		//Establecemos la constante 18
+		
+		MAYORIA_EDAD = 18;
+		
+		//Establecemos el Scanner
+		 
+		Scanner sc = new Scanner(System.in); 
+		
+		//imprimimos en pantalla la solicitud
+		System.out.println("Introduzca su edad: ");
+		Edad = sc.nextInt();
+		 
+		Edad = 18<Edad?falsa:true;
+		
+		
+
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
